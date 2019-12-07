c8f828f40f904cc4d4cbde3c0dd40e8df5ae0378
Marius Rikheim <marius.rikheim@gmail.com>
2019-12-07 02:40:43 +0100

    Task 1 completed

--- task1.py
+++ task1.py
@@ -1,11 +1,19 @@
-with open("./input.txt") as f:
-    content = f.readlines()
 
-content = [int(x.strip()) for x in content]
+def calculate_fuel(input, total) -> int:
+    tmp = (input // 3) - 2
+    if tmp <= 0:
+        return total
+    else:
+        return calculate_fuel(tmp, total + tmp)
+
+
+with open("./input.txt") as f:
+    masses = f.readlines()
+
+masses = [int(x.strip()) for x in masses]
 
 fuel = 0
-for i in content:
-    fuel += (i // 3) - 2
+for mass in masses:
+    fuel += calculate_fuel(mass, 0)
        
 print(fuel)
-
